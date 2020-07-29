70bae4450142bb98ad526cd85f423cb040946bd5
ajmal250 <60932180+ajmal250@users.noreply.github.com>
2020-07-29 18:39:00 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,8 +1,8 @@
 #Suppose you have a sequence consisting of L’s and M’s where L denotes a descending and M denotes an ascending sequence. Take a sequence as an input and decode it to construct a number without repeated digits.
 #(For example:
 #MMLLMLML -> 125437698
-#MMMM -> 54321
-#LLLL-> 12345 )
+#MMMM -> 12345
+#LLLL-> 54321 )
 #read word
 word=input("Enter the word :")
 #sequence counter asequence for ascending and dsequence for descending
